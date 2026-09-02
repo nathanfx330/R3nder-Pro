8766a8035ae9c85df28c47b84215ad0a3328b4a1
Nathaniel Westveer <nathaniel.westveer@gmail.com>
2026-09-02 06:58:45 -0400

    Remove video timing state

--- lib/scene_state.dart
+++ lib/scene_state.dart
@@ -19,9 +19,6 @@ class _ActiveGallery {
   final int videoFpsDenominator;
 
   int imageIndex = 0;
-  int framesIntoPhase = 0;
-  bool videoPlaybackStarted = false;
-  int videoFrameAccumulator = 0;
 
   _ActiveGallery({
     required this.images,
