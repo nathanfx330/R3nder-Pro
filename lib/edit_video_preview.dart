00fd51f6146676df86eb15b0f0a6d114803e69b9
Nathaniel Westveer <nathaniel.westveer@gmail.com>
2026-09-03 16:15:36 -0400

    Import ValueListenable for edit preview

--- lib/edit_video_preview.dart
+++ lib/edit_video_preview.dart
@@ -14,6 +14,7 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
