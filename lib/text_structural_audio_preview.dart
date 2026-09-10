244747be1b2b16deceeb8557b177afa5e4613c22
Nathaniel Westveer <nathaniel.westveer@gmail.com>
2026-09-10 18:25:48 -0400

    Expose workspace media resolver to TEXT audio

--- lib/text_structural_audio_preview.dart
+++ lib/text_structural_audio_preview.dart
@@ -18,6 +18,8 @@ import 'scene_engine.dart';
 import 'structural_audio_plan.dart';
 import 'structural_sequence.dart';
 
+export 'edit_video_preview.dart' show resolveWorkspaceMediaSource;
+
 class TextStructuralAudioPreview {
   ProgramStructuralAudioPreviewSession? _session;
   String? _backendName;
