27733cc436df99f91b7e7906c472cd1bf39a8dd2
Nathaniel Westveer <nathaniel.westveer@gmail.com>
2026-09-18 13:06:23 -0400

    Expect editorial defaults for new sidecards

--- test/edit_sidecard_cue_controls_test.dart
+++ test/edit_sidecard_cue_controls_test.dart
@@ -5,6 +5,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:r3nder/edit_card_cue_controls.dart';
 import 'package:r3nder/edit_cue.dart';
 import 'package:r3nder/edit_surface_model.dart';
+import 'package:r3nder/presentation_panel_content.dart';
 import 'package:r3nder/presentation_requests.dart';
 import 'package:r3nder/ui_theme.dart';
 
@@ -92,6 +93,11 @@ void main() {
     expect(added, isA<SideCardRequest>());
     expect(added!.image, 'person.png');
     expect(added!.heading, 'JOHN SMITH');
-    expect(added!.body, 'Biography text.');
+    final PresentationPanelContent panel = parsePresentationPanelContent(
+      heading: added!.heading,
+      body: added!.body,
+    );
+    expect(panel.preset, PresentationPanelPreset.editorial);
+    expect(panel.body, 'Biography text.');
   });
 }
