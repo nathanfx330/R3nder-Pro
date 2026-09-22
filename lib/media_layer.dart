847ea41049212ca36fc84a36bacf11f100fcb08b
Nathaniel Westveer <nathaniel.westveer@gmail.com>
2026-09-22 13:27:36 -0400

    Instrument raster decoder resize churn

--- lib/media_layer.dart
+++ lib/media_layer.dart
@@ -320,6 +320,9 @@ class MediaLayer {
       return existing;
     }
 
+    stderr.writeln(
+      '[RASTER_DECODER_OPEN] $resolvedPath ${width}x$height',
+    );
     final MediaDecoder created = backend.open(resolvedPath);
     _decoders[key] = created;
     _decoderLastUse[key] = ++_decoderUseSerial;
@@ -352,6 +355,10 @@ class MediaLayer {
       if (remaining <= _maxRasterVariantsPerSource) break;
       final MediaDecoder? decoder = _decoders.remove(key);
       _decoderLastUse.remove(key);
+      stderr.writeln(
+        '[RASTER_DECODER_EVICT] ${key.resolvedPath} '
+        '${key.width}x${key.height}',
+      );
       decoder?.dispose();
       remaining--;
     }
