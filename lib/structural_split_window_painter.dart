f003027105cda754aa1072f8f417b82908249b3a
Nathaniel Westveer <nathaniel.westveer@gmail.com>
2026-09-22 23:23:23 -0400

    Restore normal split image filtering

--- lib/structural_split_window_painter.dart
+++ lib/structural_split_window_painter.dart
@@ -117,8 +117,7 @@ class StructuralSplitWindowPainter extends CustomPainter {
         handoffSlideT: 1.0,
         opacity: opacity,
         windowChrome: 1.0,
-        imageFilterQuality:
-            showSourceFrameProbe ? FilterQuality.none : FilterQuality.low,
+        imageFilterQuality: FilterQuality.low,
       );
       if (showSourceFrameProbe) {
         final TextPainter probe = TextPainter(
