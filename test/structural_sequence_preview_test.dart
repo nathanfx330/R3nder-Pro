91101ec40d98a4f1f2208ddba29f220a0f48e9fd
Nathaniel Westveer <nathaniel.westveer@gmail.com>
2026-09-22 22:49:40 -0400

    Assert detached split close snapshot path

--- test/structural_sequence_preview_test.dart
+++ test/structural_sequence_preview_test.dart
@@ -343,6 +343,7 @@ void main() {
       expect(split.sourceFrame, placement.sourceDurationFrames - 1);
       expect(split.moving, isTrue);
       expect(split.holdRaster, isFalse);
+      expect(split.useCloseSnapshot, isTrue);
       expect(split.exitProgress, isNull);
       expect(
         split.entryProgress,
